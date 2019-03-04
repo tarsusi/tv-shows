d94bd574dab6bb1d130afff3d78dfc50ece45ebd
Omer Karatas <omer.karatas@commencis.com>
2019-03-04 23:15:30 +0300

    add AppHeader component

---
+++ src/components/app-header/AppHeader.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AppHeader extends Component {
+  render() {
+    return <header className="app-header">Batman TV Shows</header>;
+  }
+}
+
+export default AppHeader;
