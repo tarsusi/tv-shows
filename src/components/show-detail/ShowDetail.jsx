4b075b7f752d8870c63a9047ca11312af7515ce0
Omer Karatas <omer.karatas@commencis.com>
2019-03-04 23:16:18 +0300

    add ShowDetail component

---
+++ src/components/show-detail/ShowDetail.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ShowDetail extends Component {
+  render() {
+    return <div className="show-detail-container">Show Detail</div>;
+  }
+}
+
+export default ShowDetail;
