c49c6682f9bf820bb27ab3d4e4cea26f40d9a716
Omer Karatas <omer.karatas@commencis.com>
2019-03-04 23:16:00 +0300

    add ShowList component

---
+++ src/components/show-list/ShowList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ShowList extends Component {
+  render() {
+    return <div className="show-list-container">Show List</div>;
+  }
+}
+
+export default ShowList;
