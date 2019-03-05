fbc86d27d06f219f39284eb214390caf30111d7e
Omer Karatas <omer.karatas@commencis.com>
2019-03-05 21:50:38 +0300

    add redux devtools extension

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -1,7 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import promise from "redux-promise-middleware";
 import rootReducer from "state/reducers/rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export default function configureStore() {
-  return createStore(rootReducer, {}, applyMiddleware(promise));
+  return createStore(
+    rootReducer,
+    {},
+    composeWithDevTools(applyMiddleware(promise))
+  );
 }
