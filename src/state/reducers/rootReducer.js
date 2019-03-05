0c2deb8ae45f20f0550e51463cd37f2ec300bd1b
Omer Karatas <omer.karatas@commencis.com>
2019-03-05 23:05:45 +0300

    add tvShowDetail reducer to rootReducer

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
+import tvShowDetail from "./tvShowDetailReducer";
 import tvShowList from "./tvShowListReducer";
 
 export default combineReducers({
+  tvShowDetail,
   tvShowList
 });
