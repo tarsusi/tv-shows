104ab3256e54d674bea41b3accc3527318a8f3d4
Omer Karatas <omer.karatas@commencis.com>
2019-03-05 23:26:05 +0300

    add go back button to AppHeader

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+import { SHOW_LIST_PATH } from "common/constants/rootNames";
+
+const HEADER_TITLE = "Batman TV Shows";
+const GO_BACK_BUTTON_LABEL = "Go Show List";
 
 class AppHeader extends Component {
   render() {
-    return <header className="app-header">Batman TV Shows</header>;
+    const isUserInDetailPage = this.props.location.pathname !== SHOW_LIST_PATH;
+
+    return (
+      <header className="app-header">
+        {isUserInDetailPage && (
+          <button className="back-button" onClick={this.props.history.goBack}>
+            {GO_BACK_BUTTON_LABEL}
+          </button>
+        )}
+        {HEADER_TITLE}
+      </header>
+    );
   }
 }
 
-export default AppHeader;
+export default withRouter(AppHeader);
