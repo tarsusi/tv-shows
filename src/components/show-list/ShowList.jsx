712287f0a159e8965853edf497bbed73b0a6dafb
Omer Karatas <omer.karatas@commencis.com>
2019-03-05 22:02:16 +0300

    update ShowList component with response of search API

--- src/components/show-list/ShowList.jsx
+++ src/components/show-list/ShowList.jsx
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
 
+const TV_SHOW_NAME = "batman";
+
 class ShowList extends Component {
+  componentDidMount() {
+    this.props.fetchShows(TV_SHOW_NAME);
+  }
+
   render() {
-    return <div className="show-list-container">Show List</div>;
+    const { showList } = this.props;
+
+    return (
+      <div className="show-list-container">
+        {showList.map(show => (
+          <div className="show-item" key={show.id}>
+            <div className="show-poster">
+              <img src={show.poster} alt={show.name} />
+            </div>
+            <div className="show-preview-data">
+              <div className="show-name">{show.name}</div>
+              <div className="show-score">
+                Score: {Number(show.score).toFixed(2)}
+              </div>
+              <div className="show-rate">
+                Rate: {Number(show.rate).toFixed(2)}
+              </div>
+            </div>
+            <button
+              className="show-detail-button"
+              onClick={() => {
+                this.props.history.push(`/${show.id}`);
+              }}
+            >
+              Go Details
+            </button>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
